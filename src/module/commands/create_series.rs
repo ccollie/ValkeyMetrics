c791109b64bb7335f9401511a48317c8ff8696cb
ccollie <gboyah@gmail.com>
2024-12-26 12:30:58 -0500

    Change signature of `vm.create-series`

--- src/module/commands/create_series.rs
+++ src/module/commands/create_series.rs
@@ -1,3 +1,4 @@
+use metricsql_common::label::Label;
 use crate::error_consts;
 use crate::module::arg_parse::*;
 use crate::module::VKM_SERIES_TYPE;
@@ -11,7 +12,9 @@ use valkey_module::{
 
 /// Create a new time series
 ///
-/// VM.CREATE-SERIES key metric
+/// VM.CREATE-SERIES key
+///   [METRIC metric]
+///   [LABELS label1=value1 label2=value2 ...]
 ///   [RETENTION retentionPeriod]
 ///   [COMPRESSION <pco|gorilla|uncompressed>]
 ///   [CHUNK_SIZE chunkSize]
@@ -26,10 +29,27 @@ pub fn create(ctx: &Context, args: Vec<ValkeyString>) -> ValkeyResult {
     VALKEY_OK
 }
 
+const CREATE_ARGS: [&str; 9] = [
+    CMD_ARG_METRIC,
+    CMD_ARG_LABELS,
+    CMD_ARG_RETENTION,
+    CMD_ARG_COMPRESSION,
+    CMD_ARG_CHUNK_SIZE,
+    CMD_ARG_DEDUPE_INTERVAL,
+    CMD_ARG_DUPLICATE_POLICY,
+    CMD_ARG_SIGNIFICANT_DIGITS,
+    CMD_ARG_DECIMAL_DIGITS,
+];
+
+fn is_valid_command_arg(arg: &str) -> bool {
+    CREATE_ARGS.iter().any(|x| x.eq_ignore_ascii_case(arg))
+}
+
 pub fn parse_create_options(
     args: Vec<ValkeyString>,
 ) -> ValkeyResult<(ValkeyString, TimeSeriesOptions)> {
     let mut args = args.into_iter().skip(1).peekable();
+    let mut metric_set = false;
 
     let mut options = TimeSeriesOptions::default();
 
@@ -37,13 +57,23 @@ pub fn parse_create_options(
         .next()
         .ok_or(ValkeyError::Str("Err missing key argument"))?;
 
-    let metric = args.next_string()?;
-    options.labels =
-        parse_metric_name(&metric).map_err(|_e| ValkeyError::Str(error_consts::INVALID_METRIC))?;
-
     while let Ok(arg) = args.next_str() {
         let arg_upper = arg.to_ascii_uppercase();
         match arg_upper.as_str() {
+            CMD_ARG_METRIC => {
+                if metric_set {
+                    return Err(ValkeyError::Str(error_consts::METRIC_ALREADY_SET));
+                }
+                let metric = args.next_string()?;
+                options.labels =
+                    parse_metric_name(&metric).map_err(|_e| ValkeyError::Str(error_consts::INVALID_METRIC))?;
+            }
+            CMD_ARG_LABELS => {
+                if metric_set {
+                    return Err(ValkeyError::Str(error_consts::METRIC_ALREADY_SET));
+                }
+                options.labels = parse_labels(&mut args)?;
+            }
             CMD_ARG_RETENTION => options.retention(parse_retention(&mut args)?),
             CMD_ARG_DEDUPE_INTERVAL => {
                 options.dedupe_interval = Some(parse_dedupe_interval(&mut args)?)
@@ -77,7 +107,20 @@ pub fn parse_create_options(
         };
     }
 
+    if options.labels.is_empty() {
+        return Err(ValkeyError::Str(error_consts::INVALID_OR_MISSING_METRIC_NAME));
+    }
+
     Ok((key, options))
+}
+
+fn parse_labels(args: &mut CommandArgIterator) -> ValkeyResult<Vec<Label>> {
+    let label_map = parse_key_value_pairs(args, is_valid_command_arg)?;
+    let mut labels = Vec::new();
+    for (label_name, label_value) in label_map {
+        labels.push(Label::new(label_name, label_value));
+    }
+    Ok(labels)
 }
 
 pub(crate) fn create_series(
